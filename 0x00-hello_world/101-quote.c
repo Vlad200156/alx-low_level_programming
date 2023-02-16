9c6210517865be220b6849dbedd88817a965a08a
Kuoribo George <georgekuri456@gmail.com>
2023-02-16 06:31:48 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 /**
- * main - Prints "and that piece of art is useful" - - Dora Korpar
- * followed by a new line, to standard error.
- * Return: 1 always
+ * main - prints to string
+ *
+ * Description: Prints "and that piece of art is useful..." without puts
+ *
+ * Return: 1
  */
 int main(void)
-{	
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
 return (1);
 }
